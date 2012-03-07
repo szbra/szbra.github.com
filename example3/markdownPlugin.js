02b75f4555fce506b3b4593892c03f5ac9777bdb
Szymon Brandys <Szymon.Brandys@pl.ibm.com>
2012-03-07 15:30:58 +0100

    Sample editor action added to Example 3

---
+++ example3/markdownPlugin.js
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+/*jslint forin:true regexp:false*/
+/*global eclipse window */
+
+window.onload = function() {
+					
+	// create the plugin
+	var provider = new eclipse.PluginProvider();
+
+	var sampleMarkdownContent = 
+		"# Heading 1 \r\n" +
+		"## Sub-Heading 1.1 \r\n" +
+		"### Sub-Sub Heading 1.1.1 \r\n" +
+		"## Sub-Heading 1.2 \r\n" +
+		"# Heading 2 \r\n" +
+		"# Heading 3 \r\n";
+
+	var serviceImpl = {
+		run : function(text) {
+		
+			return text + "\r\n" + sampleMarkdownContent;
+		}
+	};
+	
+	var serviceProps = {
+		name : "Add sample markdown"
+	};
+	
+	provider.registerServiceProvider("orion.edit.command", serviceImpl, serviceProps);
+	
+	provider.connect();
+};
