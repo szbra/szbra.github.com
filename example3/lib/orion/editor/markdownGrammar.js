2ded48171cac28d93a8bad7a2367c3f855ea9ca3
Szymon Brandys <szymon.brandys@pl.ibm.com>
2012-03-12 17:34:08 +0100

    Changes to the Markdown grammar

--- example3/lib/orion/editor/markdownGrammar.js
+++ example3/lib/orion/editor/markdownGrammar.js
@@ -15,82 +15,82 @@
 define("orion/editor/MarkdownGrammar", [], function() {
 
 	/**
-	 * Provides a grammar that can do some very rough syntax highlighting for HTML.
-	 * @class orion.syntax.HtmlGrammar
+	 * Provides a grammar that can do some very rough syntax highlighting for Markdown.
+	 * @name orion.editor.MarkdownGrammar
 	 */
 	return function MarkdownGrammar() {
 		/**
 		 * Object containing the grammar rules.
-		 * @public
-		 * @type Object
 		 */
-		return {
-			"name": "HTML",
-			"scopeName": "source.html",
-			"uuid": "3B5C76FB-EBB5-D930-F40C-047D082CE99B",
-			"patterns": [
-				{
-					"begin": "<!(doctype|DOCTYPE)",
-					"end": ">",
-					"contentName": "entity.name.tag.doctype.html",
-					"beginCaptures": {
-						"0": { "name": "entity.name.tag.doctype.html" }
-					},
-					"endCaptures": {
-						"0": { "name": "entity.name.tag.doctype.html" }
+		 this.name = "Markdown";
+		 
+		 this.patterns = [
+			// markdown grammar
+			{
+				"match": "^#.*$",
+				"name": "entity.name.tag.doctype.html"
+			},
+			{
+				"begin": "^$",
+				"end": "^$",
+				"patterns": [
+					{ "match": "> .+", "name": "entity.name.tag.doctype.html" },
+					{ "match": "( {3,}|\t).+", "name": "entity.name.tag.doctype.html" }
+				]
+			},
+			// inline HTML grammar
+			{
+				"begin": "<!--",
+				"end": "-->",
+				"beginCaptures": {
+					"0": { "name": "punctuation.definition.comment.html" }
+				},
+				"endCaptures": {
+					"0": { "name": "punctuation.definition.comment.html" }
+				},
+				"patterns": [
+					{
+						"match": "--",
+						"name": "invalid.illegal.badcomment.html"
 					}
-				},
-				{
-					"begin": "<!--",
-					"end": "-->",
-					"beginCaptures": {
-						"0": { "name": "punctuation.definition.comment.html" }
-					},
-					"endCaptures": {
-						"0": { "name": "punctuation.definition.comment.html" }
-					},
-					"patterns": [
-						{
-							"match": "--",
-							"name": "invalid.illegal.badcomment.html"
-						}
-					],
-					"contentName": "comment.block.html"
-				},
-				{ // startDelimiter + tagName
-					"match": "<[A-Za-z0-9_\\-:]+(?= ?)",
-					"name": "entity.name.tag.html"
-				},
-				{ "include": "#attrName" },
-				{ "include": "#qString" },
-				{ "include": "#qqString" },
-				{ "include": "#entity" },
-				// TODO attrName, qString, qqString should be applied first while inside a tag
-				{ // startDelimiter + slash + tagName + endDelimiter
-					"match": "</[A-Za-z0-9_\\-:]+>",
-					"name": "entity.name.tag.html"
-				},
-				{ // end delimiter of open tag
-					"match": ">", 
-					"name": "entity.name.tag.html"
-				} ],
-			"repository": {
-				"attrName": { // attribute name
-					"match": "[A-Za-z\\-:]+(?=\\s*=\\s*['\"])",
-					"name": "entity.other.attribute.name.html"
-				},
-				"qqString": { // double quoted string
-					"match": "(\")[^\"]+(\")",
-					"name": "string.quoted.double.html"
-				},
-				"qString": { // single quoted string
-					"match": "(')[^']+(\')",
-					"name": "string.quoted.single.html"
-				},
-				"entity": {
-					"match": "&[A-Za-z0-9]+;",
-					"name": "constant.character.entity.html"
-				}
+				],
+				"contentName": "comment.block.html"
+			},
+			{ // startDelimiter + tagName
+				"match": "<[A-Za-z0-9_\\-:]+(?= ?)",
+				"name": "entity.name.tag.html"
+			},
+			{ "include": "#attrName" },
+			{ "include": "#qString" },
+			{ "include": "#qqString" },
+			{ "include": "#entity" },
+			// TODO attrName, qString, qqString should be applied first while inside a tag
+			{ // startDelimiter + slash + tagName + endDelimiter
+				"match": "</[A-Za-z0-9_\\-:]+>",
+				"name": "entity.name.tag.html"
+			},
+			{ // end delimiter of open tag
+				"match": ">", 
+				"name": "entity.name.tag.html"
+			} 
+		];
+
+		this.repository = {
+			"attrName": { // attribute name
+				"match": "[A-Za-z\\-:]+(?=\\s*=\\s*['\"])",
+				"name": "entity.other.attribute.name.html"
+			},
+			"qqString": { // double quoted string
+				"match": "(\")[^\"]+(\")",
+				"name": "string.quoted.double.html"
+			},
+			"qString": { // single quoted string
+				"match": "(')[^']+(\')",
+				"name": "string.quoted.single.html"
+			},
+			"entity": {
+				"match": "&[A-Za-z0-9]+;",
+				"name": "constant.character.entity.html"
 			}
 		};
 	};
